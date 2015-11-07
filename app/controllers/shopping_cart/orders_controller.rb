baaf94615761d7fff7c0f43aae48e53d8e4d342e
Max Grechko <max@grechko.org>
2015-11-07 16:21:43 +0200

    Add orders history

---
+++ app/controllers/shopping_cart/orders_controller.rb
@@ -0,0 +1,17 @@
+require_dependency "shopping_cart/application_controller"
+
+module ShoppingCart
+  class OrdersController < ApplicationController
+    def index
+      orders = current_user.orders
+      @in_progress = orders.in_progress.first
+      @in_queue = orders.in_queue
+      @in_delivery = orders.in_delivery
+      @delivered = orders.delivered
+    end
+
+    def show
+      @order = current_user.orders.find(params[:id])
+    end
+  end
+end
---
+++ spec/controllers/shopping_cart/orders_controller_spec.rb
@@ -0,0 +1,58 @@
+module ShoppingCart
+  RSpec.describe OrdersController, type: :controller do
+    routes { ShoppingCart::Engine.routes }
+
+    sign_in_user
+
+    let(:in_progress) { create(:shopping_cart_order, state: 'in_progress',user: @user) }
+    let(:in_queue) { create(:shopping_cart_order, state: 'in_queue', user: @user) }
+    let(:in_delivery) { create(:shopping_cart_order, state: 'in_delivery', user: @user) }
+    let(:delivered) { create(:shopping_cart_order, state: 'delivered', user: @user) }
+
+    describe 'GET #index' do
+      before { get :index }
+
+      it 'responds successfully with an HTTP 200 status code' do
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the first order with in_progress state' do
+        expect(assigns(:in_progress)).to eq @user.orders.in_progress.first
+      end
+
+      it 'fills an array of orders with an in_queue state' do
+        expect(assigns(:in_queue)).to match_array [in_queue]
+      end
+
+      it 'fills an array of orders with an in_delivery state' do
+        expect(assigns(:in_delivery)).to match_array [in_delivery]
+      end
+
+      it 'fills an array of orders with an delivered state' do
+        expect(assigns(:delivered)).to match_array [delivered]
+      end
+
+      it 'renders index view' do
+        expect(response).to render_template :index
+      end
+    end
+
+    describe 'GET #show' do
+      before { get :show, id: delivered }
+
+      it 'responds successfully with an HTTP 200 status code' do
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
+
+      it 'assigns the requested order to @order' do
+        expect(assigns(:order)).to eq delivered
+      end
+
+      it 'renders show view' do
+        expect(response).to render_template :show
+      end
+    end
+  end
+end
